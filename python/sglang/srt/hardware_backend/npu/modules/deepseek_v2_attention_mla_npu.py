5a07a143dbe0a0f6a838311b82033c62e5301f04
alanhe151220037 <812589971@qq.com>
2026-03-09 17:45:41 +0800

    fix dpsk atten_tp = 1 output wrong Bug

--- python/sglang/srt/hardware_backend/npu/modules/deepseek_v2_attention_mla_npu.py
+++ python/sglang/srt/hardware_backend/npu/modules/deepseek_v2_attention_mla_npu.py
@@ -280,18 +280,19 @@ def forward_mla_core_npu(
 
     attn_output = attn_output.view(-1, m.num_local_heads, m.kv_lora_rank)
 
-    # attn_bmm_output = torch.empty(
-    #     (attn_output.shape[0], m.num_local_heads, m.v_head_dim),
-    #     dtype=attn_output.dtype,
-    #     device=attn_output.device,
-    # )
+    attn_bmm_output = torch.empty(
+        (attn_output.shape[0], m.num_local_heads, m.v_head_dim),
+        dtype=attn_output.dtype,
+        device=attn_output.device,
+    )
 
     attn_output = attn_output.contiguous()
-    # torch.ops.npu.batch_matmul_transpose(attn_output, m.w_vc, attn_bmm_output)
+    torch.ops.npu.batch_matmul_transpose(attn_output, m.w_vc, attn_bmm_output)
 
-    # attn_bmm_output = attn_bmm_output.reshape(-1, m.num_local_heads * m.v_head_dim)
+    attn_bmm_output = attn_bmm_output.reshape(-1, m.num_local_heads * m.v_head_dim)
 
-    attn_bmm_output = custom_ops_qujing.npu_bmm_transpose(attn_output, m.w_vc).view(-1, m.num_local_heads * m.v_head_dim)
+    # TODO: w_vc too big may case Bug
+    # attn_bmm_output = custom_ops_qujing.npu_bmm_transpose(attn_output, m.w_vc).view(-1, m.num_local_heads * m.v_head_dim)
 
     output, _ = m.o_proj(attn_bmm_output)
 
